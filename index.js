a65ab716e1c500d9cd7bcd11828b71054a34e02f
Amjad Alkhatib <amjad.mo.kh@gmail.com>
2022-04-18 03:40:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, string){
+    return arr.filter((arr) => {return arr.toLowerCase() === string.toLowerCase()})
+}
+
+function fuzzyMatch(arr, string){
+    return arr.filter((arr) => {return arr[0] === string[0];})
+}
+
+function matchName(arr, string){
+    return arr.filter((arr) => {return arr.name === string;})
+}
